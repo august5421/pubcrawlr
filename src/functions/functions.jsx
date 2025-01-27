fba95a49f7cae41a9e8a08b68324dc390c2dd010
martinw13 <martinwatkins129@gmail.com>
2025-01-27 12:29:21 -0500

    basic directions feature, minor cleanup

--- src/functions/functions.jsx
+++ src/functions/functions.jsx
@@ -1,30 +1,30 @@
 export function darkenColor(color, amount) {
-    const col = parseInt(color.slice(1), 16); 
-    const r = Math.max(0, Math.min(255, (col >> 16) - amount * 255));
-    const g = Math.max(0, Math.min(255, ((col >> 8) & 0x00ff) - amount * 255));
-    const b = Math.max(0, Math.min(255, (col & 0x0000ff) - amount * 255));
-    return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
-  }
+  const col = parseInt(color.slice(1), 16);
+  const r = Math.max(0, Math.min(255, (col >> 16) - amount * 255));
+  const g = Math.max(0, Math.min(255, ((col >> 8) & 0x00ff) - amount * 255));
+  const b = Math.max(0, Math.min(255, (col & 0x0000ff) - amount * 255));
+  return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
+}
 
 export function haversineDistance(lat1, lon1, lat2, lon2) {
-  const R = 6371; 
+  const R = 6371;
   const dLat = (lat2 - lat1) * (Math.PI / 180);
   const dLon = (lon2 - lon1) * (Math.PI / 180);
   const a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
     Math.cos(lat1 * (Math.PI / 180)) *
-      Math.cos(lat2 * (Math.PI / 180)) *
-      Math.sin(dLon / 2) *
-      Math.sin(dLon / 2);
+    Math.cos(lat2 * (Math.PI / 180)) *
+    Math.sin(dLon / 2) *
+    Math.sin(dLon / 2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  return R * c; 
+  return R * c;
 };
 
 export function stringAvatar(name) {
-  const nameParts = name.trim().split(' ');  
-  const initials = nameParts.length > 1 
+  const nameParts = name.trim().split(' ');
+  const initials = nameParts.length > 1
     ? `${nameParts[0][0]}${nameParts[1][0]}`
-    : nameParts[0] ? nameParts[0][0] : '';  
+    : nameParts[0] ? nameParts[0][0] : '';
   return initials.toUpperCase();
 }
 
@@ -44,7 +44,7 @@ export function getGreeting() {
 export function getWalkingTime(origin, destination) {
   return new Promise((resolve, reject) => {
     const directionsService = new window.google.maps.DirectionsService();
-    
+
     directionsService.route(
       {
         origin,
@@ -53,7 +53,7 @@ export function getWalkingTime(origin, destination) {
       },
       (result, status) => {
         if (status === "OK") {
-          const walkingTime = result.routes[0].legs[0].duration.value; 
+          const walkingTime = result.routes[0].legs[0].duration.value;
           resolve(walkingTime);
         } else {
           reject("Error calculating walking time");
@@ -68,4 +68,16 @@ export function convertToLatLngLiteral(loc) {
     lat: loc.latitude,
     lng: loc.longitude,
   };
+}
+
+export function getMarkerHTML(imageUrl, name, rating, price_level, place_id, includeAddBtn) {
+  return `<div style="display: flex; flex-direction: column; align-items: left;">
+            ${imageUrl ? `<img src="${imageUrl}" alt="${name}" style="width: 200px; padding: 0px; height: 150px; object-fit: cover;" />` : ''}
+            <div style="font-weight: bold; font-size: 16px; margin: 8px;">
+              ${name.length > 38 ? `${name.slice(0, 20)}...` : name}
+            </div>
+            <div style="font-size: 14px; margin-left: 8px;">Rating: ${rating || "N/A"}</div>
+            <div style="font-size: 14px; margin-left: 8px;">Price: ${price_level ? "$".repeat(price_level) : "N/A"}</div>
+            ${includeAddBtn ? `<button id="pa-${place_id}" class="popup-btn" onclick="addBar('${place_id}')">Add</button>` : ''}
+          </div>`;
 }
--- src/pages/SingleCrawlPage.jsx
+++ src/pages/SingleCrawlPage.jsx
@@ -4,109 +4,122 @@ import { useDispatch, useSelector } from 'react-redux';
 import FinishedBarList from '../components/FinishedBarList';
 import { useParams } from "react-router-dom";
 import { getBarCrawl } from '../services/BarCrawlService';
+import { getMarkerHTML } from '../functions/functions';
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
+import MapLibreGlDirections, { LoadingIndicatorControl } from "@maplibre/maplibre-gl-directions";
 
 function SingleCrawlPage() {
-    const dispatch = useDispatch();
-    const theme = useSelector((state) => state.theme);
-    const isMobile = useSelector((state) => state.isMobile);
-    const isLarge = useSelector((state) => state.isLarge);
-    const location = useSelector((state) => state.location);
+  const dispatch = useDispatch();
+  const theme = useSelector((state) => state.theme);
+  const isMobile = useSelector((state) => state.isMobile);
+  const isLarge = useSelector((state) => state.isLarge);
+  const location = useSelector((state) => state.location);
 
-    const [crawl, setCrawl] = useState([]);
-    const [mapLoaded, setMapLoaded] = useState(false);
-    const [crawlLoaded, setCrawlLoaded] = useState(false);
-    const [map, setMap] = useState(null);
-    const { slug } = useParams();
+  const [crawl, setCrawl] = useState([]);
+  const [mapLoaded, setMapLoaded] = useState(false);
+  const [crawlLoaded, setCrawlLoaded] = useState(false);
+  const [map, setMap] = useState(null);
+  const [directions, setDirections] = useState(null);
+  const { slug } = useParams();
 
-    // load crawl by id
-    useEffect(() => {
-        getBarCrawl(slug).then((response) => {
-            setCrawl(response);
-            setCrawlLoaded(true);
+  // load crawl by id
+  useEffect(() => {
+    getBarCrawl(slug).then((response) => {
+      setCrawl(response);
+      setCrawlLoaded(true);
+    });
+  }, [slug]);
+
+  // render map
+  useEffect(() => {
+    if (!mapLoaded && crawlLoaded) {
+      if (map) return;
+
+      const centerPoint = crawl.barCrawlInfo[0];
+      const centerLngLat = [centerPoint.barLng, centerPoint.barLat];
+      const newMap = new maplibregl.Map({
+        container: 'map',
+        style: `https://api.maptiler.com/maps/openstreetmap/style.json?key=uyov68efNrdYsMZCjhLO`,
+        center: centerLngLat,
+        zoom: 12
+      });
+      newMap.addControl(new maplibregl.NavigationControl(), 'top-right');
+      let bounds = new maplibregl.LngLatBounds();
+
+      crawl.barCrawlInfo.forEach(x => {
+        bounds.extend([x.barLng, x.barLat]);
+
+        const marker = new maplibregl.Marker({
+          color: '#36b8f5',
+          className: 'marker'
+        }).setLngLat([x.barLng, x.barLat])
+          .setPopup(new maplibregl.Popup().setHTML(getMarkerHTML(x.imageUrl, x.name, x.rating, x.price_level, x.place_id, false)))
+          .addTo(newMap);
+
+        marker.getElement().addEventListener('click', () => {
+          newMap.flyTo({
+            center: [x.barLng, x.barLat],
+            speed: 0.3
+          });
         });
-    }, [slug]);
 
-    // render map
-    useEffect(() => {
-        if (!mapLoaded && crawlLoaded) {
-            if (map) return;
+        marker.getElement().setAttribute('data-placeid', x.place_id);
+      });
 
-            const centerPoint = crawl.barCrawlInfo[0];
-            const centerLngLat = [centerPoint.barLng, centerPoint.barLat];
-            const newMap = new maplibregl.Map({
-                container: 'map',
-                style: `https://api.maptiler.com/maps/openstreetmap/style.json?key=uyov68efNrdYsMZCjhLO`,
-                center: centerLngLat,
-                zoom: 12
-            });
-            newMap.addControl(new maplibregl.NavigationControl(), 'top-right');
-            let bounds = new maplibregl.LngLatBounds();
+      // zoom to fit results
+      newMap.fitBounds(bounds, {
+        padding: { top: 65, bottom: 65, left: 25, right: 25 }
+      });
 
-            crawl.barCrawlInfo.forEach(x => {
-                bounds.extend([x.barLng, x.barLat]);
+      setMap(newMap);
+      newMap.on('load', () => {
+        const newDirections = new MapLibreGlDirections(newMap, {
+          profile: 'foot'
+        });
+        newMap.addControl(new LoadingIndicatorControl(newDirections));
+        setDirections(newDirections);
+      });
+      setMapLoaded(true);
+    }
+  }, [crawlLoaded, mapLoaded]);
 
-                const marker = new maplibregl.Marker({
-                    color: '#36b8f5',
-                    className: 'marker'
-                }).setLngLat([x.barLng, x.barLat])
-                .setPopup(new maplibregl.Popup().setHTML(`<div style="display: flex; flex-direction: column; align-items: left;">
-                                                    ${x.imageUrl ? `<img src="${x.imageUrl}" alt="${x.name}" style="width: 200px; padding: 0px; height: 150px; object-fit: cover;" />` : ""}
-                                                    <div style="font-weight: bold; font-size: 16px; margin: 8px;">
-                                                      ${x.name.length > 38 ? `${x.name.slice(0, 20)}...` : x.name}
-                                                    </div>
-                                                    <div style="font-size: 14px; margin-left: 8px;">Rating: ${x.rating || "N/A"}</div>
-                                                    <div style="font-size: 14px; margin-left: 8px;">Price: ${x.price_level ? "$".repeat(x.price_level) : "N/A"}</div>
-                                                  </div>`))
-                .addTo(newMap);
+  useEffect(() => {
+    if (mapLoaded && directions) {
+      directions.setWaypoints([
+        ...crawl.barCrawlInfo.map(x => {
+          return [x.barLng, x.barLat];
+        })
+      ]);
+    }
+  }, [directions, mapLoaded]);
 
-                marker.getElement().addEventListener('click', () => {
-                    newMap.flyTo({
-                      center: [x.geometry.location.lng(), x.geometry.location.lat()],
-                      speed: 0.3
-                    });
-                });
+  return (
+    <Box
+      style={{
+        display: "flex",
+        flexDirection: isMobile ? "column" : "row",
+        height: "calc(100vh - 50px)",
+        backgroundColor: theme.white,
+        width: "100%",
+      }}
+    >
 
-                marker.getElement().setAttribute('data-placeid', x.place_id);
-            });
-
-            // zoom to fit results
-            newMap.fitBounds(bounds, {
-                padding: {top: 65, bottom:65, left: 25, right: 25}
-            });
-
-            setMap(newMap);
-            setMapLoaded(true);
-        }
-    }, [crawlLoaded, mapLoaded]);
-
-    return (
+      {!isMobile && (
         <Box
           style={{
             display: "flex",
-            flexDirection: isMobile ? "column" : "row", 
-            height: "calc(100vh - 50px)",
-            backgroundColor: theme.white,
-            width: "100%",
+            flexDirection: "column",
+            flex: isLarge ? 3 : 5,
           }}
-          >
+        >
+          <Box id="map" style={{ width: "100%", height: "calc(100vh - 50px)" }} />
+        </Box>
+      )}
+      {crawlLoaded && <FinishedBarList selectedBarCrawl={crawl} />}
+    </Box>
 
-          {!isMobile && (
-            <Box
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                flex: isLarge ? 3 : 5,
-              }}
-            >
-              <Box id="map" style={{ width: "100%", height: "calc(100vh - 50px)" }} />
-            </Box>
-          )}
-          {crawlLoaded && <FinishedBarList selectedBarCrawl={crawl} />}
-          </Box>
-    
-      );
+  );
 }
 
 export default SingleCrawlPage;
